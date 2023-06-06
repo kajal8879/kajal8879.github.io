7267484197550a3715452daf5d411b2a122f675a
Kajal Patil <kajal@pdx.edu>
2023-06-05 21:34:51 -0700

    About and work experience section

---
+++ main.js
@@ -0,0 +1,57 @@
+// Get the navbar element
+var navbar = document.getElementById("navbar");
+
+// Attach click event listeners to the navbar links
+navbar.addEventListener("click", function (event) {
+  // Check if the clicked element is a link
+  if (event.target.tagName === "A") {
+    event.preventDefault();
+
+    // Get the target section ID from the link's href attribute
+    var targetSectionId = event.target.getAttribute("href").substring(1);
+
+    // Scroll to the target section with the fixed header offset
+    scrollToElement(targetSectionId);
+  }
+});
+
+// Function to scroll to an element with header offset
+function scrollToElement(elementId) {
+  var element = document.getElementById(elementId);
+  var header = document.getElementById("header");
+  var headerHeight = header.offsetHeight;
+
+  if (element) {
+    var elementPosition = element.getBoundingClientRect().top;
+    var offsetPosition = elementPosition - headerHeight;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
+  }
+}
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+
+  if (window.pageYOffset > 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+var coll = document.getElementsByClassName("workEx-item-button");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
